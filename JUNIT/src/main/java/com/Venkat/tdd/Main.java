65c2550df57a5832aa25eeaa502360411e4d58d5
sivamanivenkat <79448310+sivamanivenkat@users.noreply.github.com>
2025-03-28 11:45:44 -0500

    Delete JUNIT directory

--- JUNIT/src/main/java/com/Venkat/tdd/Main.java
+++
@@ -1,9 +0,0 @@
-package com.Venkat.tdd;
-
-public class Main {
-    public static void main(String[] args) {
-        for(int i=0;i<=100;i++){
-            System.out.println(Fizzbuzz.compute(i));
-        }
-    }
-}
